53f3c4ec3b79015ea0a24a0840d689b5a55f74a9
Duane Walker <duane@walker-family.org>
2017-09-15 09:32:11 +1000

    Added repositories

---
+++ src/main/java/com/walker/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,8 @@
+package com.walker.spring5webapp.repositories;
+
+import com.walker.spring5webapp.model.Author;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long>{
+
+}
