8a72d21c5d16db2a7381e42b3f9c4f3185e7a14b
Duane Walker <duane@walker-family.org>
2017-09-17 21:58:47 +1000

    Added some Controllers and Views.

--- src/main/java/com/walker/spring5webapp/bootstrap/DevBootStrap.java
+++ src/main/java/com/walker/spring5webapp/bootstrap/DevBootStrap.java
@@ -2,8 +2,10 @@ package com.walker.spring5webapp.bootstrap;
 
 import com.walker.spring5webapp.model.Author;
 import com.walker.spring5webapp.model.Book;
+import com.walker.spring5webapp.model.Publisher;
 import com.walker.spring5webapp.repositories.AuthorRepository;
 import com.walker.spring5webapp.repositories.BookRepository;
+import com.walker.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent>{
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootStrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootStrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -25,8 +29,14 @@ public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent>{
     }
 
     private void initData(){
+        Publisher publisher = new Publisher("Harper Collins", "Address");
+        publisherRepository.save(publisher);
+
+        Publisher publisher2 = new Publisher("Worx", "Address");
+        publisherRepository.save(publisher2);
+
         Author eric = new Author("Eric", "Evans");
-        Book ddd = new Book("Domain Driver Design", "1234", "Harper Collins");
+        Book ddd = new Book("Domain Driver Design", "1234", publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
@@ -34,7 +44,7 @@ public class DevBootStrap implements ApplicationListener<ContextRefreshedEvent>{
         bookRepository.save(ddd);
 
         Author rod = new Author("Rod", "Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", publisher2);
         rod.getBooks().add(noEJB);
 
         authorRepository.save(rod);
---
+++ src/main/java/com/walker/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,8 @@
+package com.walker.spring5webapp.repositories;
+
+import com.walker.spring5webapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long>{
+
+}
